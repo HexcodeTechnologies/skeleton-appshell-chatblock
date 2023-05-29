396e8e06ce1a9af259764565f389c4b07f68f657
Phyo Arkar Lwin <phyo.arkarlwin@gmail.com>
2023-05-29 20:45:45 +0630

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <main class="grid grid-rows-[1fr_auto]">
-<div class="bg-blue-500 p-4 overflow-y-auto">
-	{#each Array.from({ length: 1000 }, (_, i) => i) as item}
-		<div>
-			{item}
-		</div>
-	{/each}
-</div>
+	<div class="bg-blue-500 p-4 overflow-y-auto">
+		{#each Array.from({ length: 1000 }, (_, i) => i) as item}
+			<div>
+				{item}
+			</div>
+		{/each}
+	</div>
 <footer class="bg-green-500 p-4">(footer)</footer>
 </main>
