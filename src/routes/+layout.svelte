a0181ee23607dda19e1ba24b452db52305371231
Phyo <phyoakl@hexcode.tech>
2023-05-29 17:32:59 +0630

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	let currentTile: number = 0;
+
+	import '../app.postcss';
+	import { AppShell, AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell
+	slotSidebarLeft="bg-surface-500/5"
+	slotPageContent="bg-red-500 h-full   grid grid-rows-[1fr_auto]"
+>
+	<svelte:fragment slot="header">Header</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<!-- Insert the list: -->
+		<AppRail>
+			<svelte:fragment slot="lead">
+				<AppRailAnchor href="/">(icon)</AppRailAnchor>
+			</svelte:fragment>
+			<!-- --- -->
+			<AppRailTile bind:group={currentTile} name="tile-1" value={0}>
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Tile 1</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-2" value={1}>
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Tile 2</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-3" value={2}>
+				<svelte:fragment slot="lead">(icon)</svelte:fragment>
+				<span>Tile 3</span>
+			</AppRailTile>
+			<!-- --- -->
+			<svelte:fragment slot="trail">
+				<AppRailAnchor href="/" target="_blank" title="Account">(icon)</AppRailAnchor>
+			</svelte:fragment>
+		</AppRail>
+		<!-- --- -->
+	</svelte:fragment>
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<!-- (footer) -->
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="bg-blue-500 p-4 overflow-y-auto">
+	{#each Array.from({ length: 1000 }, (_, i) => i) as item}
+		<div>
+			{item}
+		</div>
+	{/each}
+</div>
+<footer class="bg-green-500 p-4">(footer)</footer>
